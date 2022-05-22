571de0bbc7b512e8d4c2a5f65f6f3093844b94b9
Billy Huang <10646003@ntub.edu.tw>
2022-05-23 00:54:39 +0800

    feat(task_page): Designing the To-Do List App

---
+++ lib/pages/task_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatelessWidget {
+  const TaskPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(
+              top: 60.0,
+              left: 30.0,
+              right: 30.0,
+              bottom: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'Todo List',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
